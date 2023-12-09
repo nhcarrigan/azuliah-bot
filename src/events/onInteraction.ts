cb6b58b69cd7487ee2040728535537e4fe597939
Naomi Carrigan <nhcarrigan@gmail.com>
2023-12-09 10:47:33 -0800

    feat: roles (#3)

--- src/events/onInteraction.ts
+++ src/events/onInteraction.ts
@@ -1,4 +1,4 @@
-import { Interaction } from "discord.js";
+import { GuildMember, Interaction } from "discord.js";
 
 import { ExtendedClient } from "../interfaces/ExtendedClient";
 import { errorHandler } from "../utils/errorHandler";
@@ -24,6 +24,38 @@ export const onInteraction = async (
       await interaction.reply({
         content: `${name} is not valid command oopsie whoopsie`,
       });
+    }
+
+    if (interaction.isButton()) {
+      if (interaction.customId.startsWith("rr-")) {
+        await interaction.deferReply({ ephemeral: true });
+        const { guild, customId, member: untypedMember } = interaction;
+        const roleId = customId.split("-")[1];
+        const role = await guild?.roles.fetch(roleId);
+        const member = untypedMember as GuildMember;
+
+        if (!role) {
+          await interaction.editReply({
+            content: "Cannot find that role. Please contact Naomi.",
+          });
+          return;
+        }
+
+        let content = "";
+
+        if (member.roles.cache.has(role.id)) {
+          const success = await member.roles.remove(role).catch(() => false);
+          content = success
+            ? `I have removed the ${role.name} role from you.`
+            : `I failed to remove that role. Please contact Naomi.`;
+        } else {
+          const success = await member.roles.add(role).catch(() => false);
+          content = success
+            ? `I have given the ${role.name} role to you.`
+            : `I failed to remove that role. Please contact Naomi.`;
+        }
+        await interaction.editReply({ content });
+      }
     }
   } catch (err) {
     await errorHandler(bot, "on interaction", err);
